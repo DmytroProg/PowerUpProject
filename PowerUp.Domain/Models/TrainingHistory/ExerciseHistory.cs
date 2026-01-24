c534fa6807ce653eabc95f8e1757c4f9cc962dc9
dmytroprog <dmytroprog@gmail.com>
2026-01-24 10:16:54 +0100

    history model

---
+++ PowerUp.Domain/Models/TrainingHistory/ExerciseHistory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PowerUp.Domain.Models.TrainingHistory;
+
+public class ExerciseHistory
+{
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(TrainingHistory))]
+    public int TrainingHistoryId { get; set; }
+    public required TrainingHistory TrainingHistory { get; set; }
+
+    public required string ExerciseState { get; set; }
+
+    public required string SetsHistory { get; set; }
+}
---
+++ PowerUp.Domain/Models/TrainingHistory/TrainingHistory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using PowerUp.Domain.Models.Users;
+
+namespace PowerUp.Domain.Models.TrainingHistory;
+
+public class TrainingHistory
+{
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+    public required User User { get; set; }
+
+    public required string TrainingState { get; set; }
+
+    public DateTime TrainingStartTime { get; set; }
+}
